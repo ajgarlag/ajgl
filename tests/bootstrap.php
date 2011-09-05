4c4f9944754582c5f5f7d8f42813c3d46da9c9ed
Antonio J. García Lagar <aj@garcialagar.es>
2011-09-05 20:37:49 +0200

    New git submodules for doctrine/common and doctrine/dbal

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -3,6 +3,8 @@
 set_include_path(implode(PATH_SEPARATOR, array(
     realpath(dirname(__FILE__) . '/../library'),
     realpath(dirname(__FILE__) . '/../vendor/ajgarlag/zf1/library'),
+    realpath(dirname(__FILE__) . '/../vendor/doctrine/common/lib'),
+    realpath(dirname(__FILE__) . '/../vendor/doctrine/dbal/lib'),
     realpath(dirname(__FILE__) . '/../vendor/guilhermeblanco/ZF1-Doctrine2-ServiceLayer/src/library'),
     get_include_path(),
 )));
@@ -14,5 +16,10 @@ $zfa = Zend_Loader_Autoloader::getInstance();
 Zend_Registry::set('Zend_Locale', new Zend_Locale('en'));
 
 $zfa->registerNamespace('Ajgl_');
+
+require_once "Doctrine/Common/ClassLoader.php";
+$autoloader = array(new \Doctrine\Common\ClassLoader('Doctrine'), 'loadClass');
+$zfa->pushAutoloader($autoloader, 'Doctrine\\');
+
 $zfa->registerNamespace('Bisna');
 
