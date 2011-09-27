7ab23fba966a0d71431fefcc91e33fb2cba7d46b
Antonio J. García Lagar <aj@garcialagar.es>
2011-09-27 19:24:44 +0200

    New form element and view helper to set dates as strings

---
+++ library/Ajgl/Form/Element/TextDate.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * AJ General Libraries
+ * Copyright (C) 2010-2011 Antonio J. García Lagar <aj@garcialagar.es>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, version 3 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @category   Ajgl
+ * @package    Ajgl_Form
+ * @subpackage Element
+ * @copyright  Copyright (C) 2010-2011 Antonio J. García Lagar <aj@garcialagar.es>
+ * @license    http://www.fsf.org/licensing/licenses/agpl-3.0.html AGPL3
+ */
+
+/**
+ * @category   Ajgl
+ * @package    Ajgl_Form
+ * @subpackage Element
+ * @copyright  Copyright (C) 2010-2011 Antonio J. García Lagar <aj@garcialagar.es>
+ * @license    http://www.fsf.org/licensing/licenses/agpl-3.0.html AGPL3
+ */
+class Ajgl_Form_Element_TextDate
+    extends Zend_Form_Element_Text
+{
+    /**
+     * Default form view helper to use for rendering
+     * @var string
+     */
+    public $helper = 'formTextDate';
+    
+    public function init()
+    {
+        $this->addValidator('Date');
+    }
+
+    public function setValue($value) {
+        if (!$value instanceof Zend_Date) {
+            $value = new Zend_Date($value, Zend_Date::DATE_SHORT);
+        }
+        return parent::setValue($value);
+    }
+    
+    /**
+     * @param Zend_View_Interface $view
+     * @return string
+     */
+    public function render(Zend_View_Interface $view = null) {
+        
+        if (null !== $view) {
+            $this->setView($view);
+            
+        }
+
+        if ($this->getView() instanceof Zend_View_Abstract) {
+            $this->getView()->addHelperPath('Ajgl/View/Helper', 'Ajgl_View_Helper');
+        }
+        
+        return parent::render($view);
+    }
+}
---
+++ tests/library/Ajgl/Form/Element/TextDateTest.php
@@ -0,0 +1,32 @@
+<?php
+class Ajgl_Form_Element_TextDateTest
+    extends PHPUnit_Framework_TestCase
+{
+    /**
+     * @var Ajgl_Form_Element_TextDate
+     */
+    protected $_element;
+    
+    public function setUp() {
+        $this->_element = new Ajgl_Form_Element_TextDate('date');
+    }
+    
+    public function testSetValue()
+    {
+        $date = '6-10-1979';
+        $this->_element->setValue($date);
+        $this->assertTrue($this->_element->getValue() instanceof Zend_Date);
+        $this->assertEquals('June', $this->_element->getValue()->get(Zend_Date::MONTH_NAME));
+        $this->assertEquals('6/10/79', $this->_element->getValue()->get(Zend_Date::DATE_SHORT));
+    }
+    
+    public function testRender()
+    {
+        $date = new Zend_Date(1234567890);
+        $this->_element->setValue($date);
+        $expected = '<dt id="date-label">&#160;</dt>' . "\n"
+            . '<dd id="date-element">' . "\n"
+            . '<input type="text" name="date" id="date" value="2/13/09"></dd>';
+        $this->assertEquals($expected, $this->_element->render(new Zend_View()));
+    }
+  }
